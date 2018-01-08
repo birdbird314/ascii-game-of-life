72bb9f7fede1a2bd5be334216b2f386b960f7fb9
birdbird314 <grantstadt@gmail.com>
2018-01-08 19:53:16 +0100

    Move file reading logic to proper class

--- src/main/java/pl/januszekodu/gameoflife/stateretriever/InitialStateRetrieverFactory.java
+++ src/main/java/pl/januszekodu/gameoflife/stateretriever/InitialStateRetrieverFactory.java
@@ -1,36 +1,11 @@
 package pl.januszekodu.gameoflife.stateretriever;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class InitialStateRetrieverFactory {
 
   private InitialStateRetrieverFactory() {
   }
 
-  private static String readFile(String path) {
-    try (BufferedReader reader = getResourceBufferedReader(path)) {
-      return readAllLines(reader);
-    } catch (IOException ioe) {
-      ioe.printStackTrace();
-      return "";
-    }
-  }
-
-  private static String readAllLines(BufferedReader reader) throws IOException {
-    String line, result = "";
-    while ((line = reader.readLine()) != null) {
-        result += line + "\n";
-    }
-    return result;
-  }
-
-  private static BufferedReader getResourceBufferedReader(String path) {
-    return new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(path)));
-  }
-
   public static InitialStateRetriever anInitialStateRetriever(Preset preset) {
-    return new InitialStateRetriever(InitialStateRetrieverFactory::readFile, preset);
+    return new InitialStateRetriever(new ResourceFileReader(), preset);
   }
 }
---
+++ src/main/java/pl/januszekodu/gameoflife/stateretriever/ResourceFileReader.java
@@ -0,0 +1,30 @@
+package pl.januszekodu.gameoflife.stateretriever;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ResourceFileReader implements FileReader{
+
+  @Override
+  public String read(String path) {
+    try (BufferedReader reader = getResourceBufferedReader(path)) {
+      return readAllLines(reader);
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+      return "";
+    }
+  }
+
+  private static String readAllLines(BufferedReader reader) throws IOException {
+    String line, result = "";
+    while ((line = reader.readLine()) != null) {
+        result += line + "\n";
+    }
+    return result;
+  }
+
+  private static BufferedReader getResourceBufferedReader(String path) {
+    return new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(path)));
+  }
+}
